998b23fb2c2a673ae60c04ebabe1781d469ff60d
Tyler Ryan <tlrrn5@gmail.com>
2022-07-17 23:55:17 -0500

    Added border thickness and gaps

--- config.h
+++ config.h
@@ -3,18 +3,21 @@
 // TODO: Fix tag bindings
 //
 /* appearance */
-static const unsigned int borderpx  = 1;        /* border pixel of windows */
-static const unsigned int snap      = 32;       /* snap pixel */
-static const int showbar            = 1;        /* 0 means no bar */
-static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[]          = { "monospace:size=10" };
-static const char dmenufont[]       = "monospace:size=10";
-static const char col_gray1[]       = "#222222";
-static const char col_gray2[]       = "#444444";
-static const char col_gray3[]       = "#bbbbbb";
-static const char col_gray4[]       = "#eeeeee";
-static const char col_cyan[]        = "#005577";
-static const char *colors[][3]      = {
+static const unsigned int borderpx   = 2;        /* border pixel of windows */
+static const unsigned int fborderpx  = 2;        /* border pixel of windows */
+static const unsigned int gappx      = 6;        /* gaps between windows */
+
+static const unsigned int snap       = 32;       /* snap pixel */
+static const int showbar             = 1;        /* 0 means no bar */
+static const int topbar              = 1;        /* 0 means bottom bar */
+static const char *fonts[]           = { "monospace:size=10" };
+static const char dmenufont[]        = "monospace:size=10";
+static const char col_gray1[]        = "#222222";
+static const char col_gray2[]        = "#444444";
+static const char col_gray3[]        = "#bbbbbb";
+static const char col_gray4[]        = "#eeeeee";
+static const char col_cyan[]         = "#005577";
+static const char *colors[][3]       = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
@@ -36,7 +39,7 @@ static const Rule rules[] = {
 /* layout(s) */
 static const float mfact     = 0.55; /* factor of master area size [0.05..0.95] */
 static const int nmaster     = 1;    /* number of clients in master area */
-static const int resizehints = 1;    /* 1 means respect size hints in tiled resizals */
+static const int resizehints = 0;    /* 1 means respect size hints in tiled resizals */
 static const int lockfullscreen = 1; /* 1 will force focus on the fullscreen window */
 
 static const Layout layouts[] = {
@@ -78,6 +81,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_d,      incnmaster,     {.i = -1 } },		// Vertical Layout
 	{ MODKEY,                       XK_u,      setmfact,       {.f = -0.05} },		// Increase Window Size
 	{ MODKEY,                       XK_o,      setmfact,       {.f = +0.05} },		// Decrease Window Size
+	// TODO: This is not enough functionality: Try with 3 windows
 	{ MODKEY|ShiftMask,             XK_j, 	   zoom,           {0} },
 	{ MODKEY,                       XK_Tab,    view,           {0} },
 	{ MODKEY,             		XK_q,      killclient,     {0} },
